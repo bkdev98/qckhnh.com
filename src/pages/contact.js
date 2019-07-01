ca1e00237bcc931b5d3cdbcc0b3b8ead2b887857
Quốc Khánh <bkdev98@gmail.com>
2019-07-01 20:30:27 +0700

    fix tracks useEffect

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,6 +9,7 @@ const ContactPage = () => (
     <p className="link-active">Quoc Khanh</p>
     <p style={{ margin: 0 }}>0949 840 370</p>
     <p style={{ margin: 0 }}>bkdev98@gmail.com</p>
+    <br />
     <p style={{ margin: 0 }}>fb.com/bkdev98</p>
     <p style={{ margin: 0 }}>github.com/bkdev98</p>
     <p style={{ margin: 0 }}>twitter.com/bkdev98</p>
--- src/pages/tracks.js
+++ src/pages/tracks.js
@@ -11,15 +11,18 @@ const TracksPage = () => {
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
 
-  useEffect(async () => {
-    setLoading(true)
-    const result = await axios.get(`${API_URL}`);
-    setLoading(false)
-    if (result.status === 200) {
-      setTracks(result.data.tracks)
-    } else {
-      setError(true)
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true)
+      const result = await axios.get(`${API_URL}`);
+      setLoading(false)
+      if (result.status === 200) {
+        setTracks(result.data.tracks)
+      } else {
+        setError(true)
+      }
     }
+    fetchData();
   }, [])
 
   return (
@@ -27,8 +30,8 @@ const TracksPage = () => {
       <SEO title="Tracks" />
       <h3>Recent music history</h3>
       {loading && <i>Crunching latest data...</i>}
-      {tracks.length > 0 && tracks.map(({ title, artist, timestamp, link, thumbs }) => {
-        const thumb = thumbs.alt ? thumbs.alt : thumbs.yt;
+      {tracks.length > 0 && tracks.map(({ key, title, artist, timestamp, link }) => {
+        // const thumb = thumbs.alt ? thumbs.alt : thumbs.yt;
         if (timestamp.nowplaying) {
           return (
             <a
@@ -37,6 +40,7 @@ const TracksPage = () => {
               className='link-active'
               target='__blank'
               href={link}
+              key={key}
             >
               {title} ({artist})
             </a>
@@ -48,6 +52,7 @@ const TracksPage = () => {
             style={{ marginRight: 20, fontStyle: 'italic', textDecoration: 'none' }}
             target='__blank'
             href={link}
+            key={key}
           >
             {title} ({artist})
           </a>
