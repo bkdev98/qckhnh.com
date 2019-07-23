1e8c8491659b90ba4a8f93c2554709279d057649
Quốc Khánh <bkdev98@gmail.com>
2019-07-23 18:02:51 +0700

    dark mode ☾

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,21 @@
 import { Link } from "gatsby"
 import React from "react"
+import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 
 const Header = () => (
   <header>
-    <Link to="/" className="title">q<span style={{ textDecoration: 'line-through' }}>uo</span>c kh<span style={{ textDecoration: 'line-through' }}>a</span>nh</Link>
+    <span className="title">
+      <Link to="/" style={{ display: 'inline-block' }}>
+        q<span style={{ textDecoration: 'line-through' }}>uo</span>c kh<span style={{ textDecoration: 'line-through' }}>a</span>nh
+      </Link>
+      <ThemeToggler>
+        {({ theme, toggleTheme }) => (
+          <span style={{ cursor: 'pointer', marginTop: 3 }} onClick={() => toggleTheme(theme === 'dark' ? 'light' : 'dark')}>
+            {theme === 'dark' ? '☀' : '☾'}
+          </span>
+        )}
+      </ThemeToggler>
+    </span>
     <ul>
       <li><Link partiallyActive={true} to="/now" activeClassName="active">now</Link></li>
       <li><Link partiallyActive={true} to="/tutorials" activeClassName="active">tutorials</Link></li>
