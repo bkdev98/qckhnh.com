3e0257b117fba338c7ed9d51371293c6ebecc336
Quốc Khánh <bkdev98@gmail.com>
2019-08-06 06:44:52 +0700

    minorities are just people. people like us.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,31 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <blockquote>
-      Encumbered forever by desire and ambition.
-      There's a hunger still unsatisfied.
+      It’s just that the cause wasn’t real.
+      The cause was imagined.
+      The cause was fear.
+      Let’s leave the Jews out o
+      this just for a moment.
+      Let’s think of another minority.
+      One that…
+      One that can go unnoticed if it needs to.
+      There are all sorts of minorities,
+      blondes for example…
+      …or people with freckles.
+      But a minority is only
+      thought of as one…
+      …when it constitutes some kind of
+      threat to the majority.
+      A real threat or an imagined one.
+      And therein lies the fear.
+      If the minority is somehow invisible…
+      …and the fear is much greater.
+      That fear is why the
+      minority is persecuted.
+      So, you see there always is a cause.
+      The cause is fear.
+      Minorities are just people.
+      People like us.
     </blockquote>
   </Layout>
 )
